09ade4753e1d4a8613e1773e2ccf95dae9484c68
Thomas Musselman <tmusselman@gmail.com>
2022-09-12 13:21:00 -0400

    feat: ride and amusement park model methods

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -1,3 +1,11 @@
 class AmusementPark < ApplicationRecord
   has_many :rides
+
+  def rides_ordered_alphabetically
+    rides.ordered_alphabetically
+  end
+
+  def rides_average_thrill
+    rides.average_thrill
+  end
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,4 +2,12 @@ class Ride < ApplicationRecord
   belongs_to :amusement_park
   has_many :ride_mechanics
   has_many :mechanics, through: :ride_mechanics
+
+  def self.ordered_alphabetically
+    order(:name)
+  end
+
+  def self.average_thrill
+    average(:thrill_rating)
+  end
 end
