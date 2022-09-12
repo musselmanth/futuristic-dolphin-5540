6ed6d2ca1bea2f571edbd116408a7b0e89a3fd41
Thomas Musselman <tmusselman@gmail.com>
2022-09-12 12:15:30 -0400

    feat: Mechanic model returns list of rides that are open and orders by thrill

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -5,4 +5,8 @@ class Mechanic < ApplicationRecord
   def self.average_experience
     average(:years_experience)
   end
+
+  def open_rides_ordered_by_thrill
+    rides.where(open: true).order(thrill_rating: :desc)
+  end
 end
